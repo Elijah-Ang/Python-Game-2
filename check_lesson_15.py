14390e1bf6cf2d14e15d271dedcb5a777bf16eb4
elijahang <elijahang77@gmail.com>
2026-01-12 01:27:36 +0800

    fixed syntax errors in the curriculum data files

--- check_lesson_15.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('/Users/elijahang/Python-Game-2/frontend/public/data/lessons.json', 'r') as f:
-    data = json.load(f)
-
-l15 = data.get('15', {})
-print(json.dumps(l15, indent=2))
